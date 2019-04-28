772813e487f623e916a4fd345001ff2f5932c501
yugamitra <34996078+yugamitra@users.noreply.github.com>
2019-04-28 12:41:37 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -4,23 +4,16 @@ var fs = require('fs');
 var app = express();
 var path = require("path");
 
-//setting middleware
-app.use('/css',express.static(__dirname +'/css'));
-app.use('/static',express.static(path.join(__dirname + 'public'))); //Serves resources from public folder
 var server = http.createServer(function (req, res) {
-
-    
-    fs.readFile('test.html', function (err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-        // res.writeHead(200, {'Content-type' : 'text/css'});
-        // var fileContents = fs.readFileSync('./stylesheet/styles.css', {encoding: 'utf8'});
-        // res.write(fileContents);
-        // res.end();
-    });
-       
-});
+require('fs').createReadStream(require('path').join(__dirname, req.url)).on('error',function(){ // static files!
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(
+      require('fs')
+        .readFileSync(require('path')
+        .join(__dirname, 'test.html') // or default to index
+      ));
+    }).pipe(res); // stream
+  });
 
 server.listen(5000);
 console.log('Server running on 5000...');
