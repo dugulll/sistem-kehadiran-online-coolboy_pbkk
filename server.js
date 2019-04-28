28320f302d45ed958a52db234ff017eb5c6d21f0
yugamitra <34996078+yugamitra@users.noreply.github.com>
2019-04-28 10:47:56 +0700

    Add files via upload
    revisi

--- server.js
+++ server.js
@@ -8,11 +8,18 @@ var path = require("path");
 app.use('/css',express.static(__dirname +'/css'));
 app.use('/static',express.static(path.join(__dirname + 'public'))); //Serves resources from public folder
 var server = http.createServer(function (req, res) {
-    fs.readFile('index.html', function (err, data) {
+
+    
+    fs.readFile('test.html', function (err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         res.end();
+        // res.writeHead(200, {'Content-type' : 'text/css'});
+        // var fileContents = fs.readFileSync('./stylesheet/styles.css', {encoding: 'utf8'});
+        // res.write(fileContents);
+        // res.end();
     });
+       
 });
 
 server.listen(5000);
